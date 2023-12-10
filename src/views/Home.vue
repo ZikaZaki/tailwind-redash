8d585f73ebee9dd78f3e4cf6556e7798a0b65ecd
ZikaZaki <17160061@su.edu.ye>
2023-12-11 01:27:32 +0300

    add CreditCard component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,9 @@
     <!-- <div class="bg-gray-300 rounded-xl h-14">search</div> -->
 
     <div class="bg-gray-300 rounded-xl mt-4 lg:hidden p-2">
-      <button class="block w-32 h-10 bg-blue-600 mx-auto" @click="showSidebar = !showSidebar">
+      <!-- <button class="block w-32 h-10 bg-blue-600 mx-auto" @click="showSidebar = !showSidebar">
         Toggle sidebar
-      </button>
+      </button> -->
     </div>
 
     <div class="overflow-hidden">
@@ -25,7 +25,10 @@
               <div class="bg-gray-300 rounded-xl h-28 w-full"></div>
             </div>
           </div>
-          <div class="w-full flex-shrink-0 lg:w-1/4 lg:flex-shrink bg-gray-300 rounded-xl h-52">sidebar</div>
+          <div class="w-full flex-shrink-0 lg:w-1/4 lg:flex-shrink bg-gray-300 rounded-xl h-52">
+            <!-- sidebar -->
+            <CreditCard />
+          </div>
         </div>
       </div>
     </div>
@@ -33,11 +36,11 @@
 </template>
 
 <script>
-
+import CreditCard from "../components/CreditCard.vue";
 
 export default {
   name: 'Home',
-  components: {},
+  components: { CreditCard },
   data: function(){
     return {
       showSidebar: false,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,21 @@
+const plugin = require("tailwindcss/plugin");
+const backfaceVisibility = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".backface-visible": {
+      "backface-visibility": "visible",
+      "-moz-backface-visibility": "visible",
+      "-webkit-backface-visibility": "visible",
+      "-ms-backface-visibility": "visible",
+    },
+    ".backface-hidden": {
+      "backface-visibility": "hidden",
+      "-moz-backface-visibility": "hidden",
+      "-webkit-backface-visibility": "hidden",
+      "-ms-backface-visibility": "hidden",
+    },
+  });
+});
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -17,6 +35,6 @@ export default {
       ],
     },
   },
-  plugins: [],
-};
 
+  plugins: [backfaceVisibility],
+};
