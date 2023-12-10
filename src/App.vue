17f3a11c25c6043ddc55e86e91cc8804521ab555
ZikaZaki <17160061@su.edu.ye>
2023-12-10 20:07:23 +0300

    add the border-radius styling to meet the design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex h-full">
+  <div class="flex h-full bg-blue-800">
     <aside>
       <!-- Main - Side-Navbar -->
       <MainSideNav />
     </aside>
 
-    <main class="w-full p-4 lg:p-10">
+    <main class="w-full p-4 lg:p-10 lg:rounded-l-3xl bg-white">
       <!-- Header - TopNavigation -->
       <header>
         <TopNav />
