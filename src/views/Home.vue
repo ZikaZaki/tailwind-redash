3329a08240f9752bf1117a15c552d3f0ef8c8eae
ZikaZaki <17160061@su.edu.ye>
2023-12-19 04:31:10 +0300

    update blog padding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
       </button>
     </div>
 
-    <div class="h-full">
+    <div class="h-full pb-52 md:pb-24">
       <div class="h-full transform lg:transform-none transition-all duration-300" :class="showSidebar ? '-translate-x-full' : ''">
         <div class="flex h-full mt-4 lg:mt-10 lg:space-x-10">
           <div class="w-full h-full overflow-auto flex-shrink-0 lg:w-[72%] lg:flex-shrink rounded-xl">
@@ -22,8 +22,10 @@
               <!-- <div class="bg-yellow-300 rounded-xl h-72 w-full md:w-[45%]"></div> -->
 
             </div>
+            
 
-            <Blog />
+              <Blog />
+
   
             <div class="flex space-x-4 lg:space-x-10 mt-4 lg:mt-10">
               <div class="bg-yellow-300 rounded-xl h-52 w-full"></div>
