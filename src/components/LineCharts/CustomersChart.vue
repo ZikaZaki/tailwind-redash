0e934fae2660562067aef03fa7dd5560fb6fa660
ZikaZaki <17160061@su.edu.ye>
2023-12-20 05:37:06 +0300

    update CustomerChart api url

--- src/components/LineCharts/CustomersChart.vue
+++ src/components/LineCharts/CustomersChart.vue
@@ -74,7 +74,8 @@ export default {
   methods: {
     async fetchData() {
       try {
-        const response = await fetch(`${import.meta.env.BASE_URL}/public/customers-data.json`);
+        console.log("BASE_URL.........: ", `${import.meta.env.BASE_URL}public/customers-data.json`)
+        const response = await fetch(`${import.meta.env.BASE_URL}public/customers-data.json`);
         const data = await response.json();
         this.chartData.labels = data.labels;
         this.chartData.datasets[0].data = data.data;
