3413ddbc51c42b4c739b1a8c44b5efab66d5a576
ZikaZaki <17160061@su.edu.ye>
2023-12-22 04:53:48 +0300

    update SpentTimeChart

--- src/components/SpentTimeChart/SpentTimeChart.vue
+++ src/components/SpentTimeChart/SpentTimeChart.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="relative overflow-hidden flex w-full lg:w-1/2 h-72 shadow-lg lg:shadow-xl bg-white border-2 border-indigo-100 rounded-xl">
+  <div class="relative overflow-hidden flex w-full lg:w-1/2 h-72 bg-white shadow-lg border-2 border-indigo-100 rounded-xl">
     <time-chart
-      :chart-data="chartData"
-      :options="chartOptions"
+      :chartType="chartType"
+      :chartData="chartData"
+      :chart-options="chartOptions"
+      :chart-title="chartTitle"
+      chart-id="spentTimeChart"
     ></time-chart>
   </div>
 </template>
@@ -10,100 +13,124 @@
 <script>
 import TimeChart from "./TimeChart.vue";
 
+const type = "line";
+const title = "Spent Time";
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  layout: { padding: 0 },
+  scales: {
+    x: {
+      display: false,
+      grid: { display: false, },
+      ticks: {
+        color: "rgba(0, 0, 0, 0.5)",
+        font: {
+          size: 12,
+          weight: 600,
+        },
+      },
+    },
+    y: {
+      beginAtZero: true,
+      display: false,
+    },
+  },
+  plugins: {
+    legend: { display: false },
+    title: {
+      display: true,
+      fullSize: true,
+      align: "start",
+      text: "     " + title,
+      color: "#000000",
+      font: {
+        size: 16,
+        weight: "bold",
+      },
+      padding: 10,
+    },
+    tooltip: {
+      position: "nearest",
+      interaction: { intersect: false },
+      callbacks: {
+        label: (context) => {
+          const value = context.parsed.y;
+          const hours = Math.floor(value);
+          const minutes = Math.round((value - hours) * 60);
+          return ` ${hours}h ${minutes}min`;
+        },
+      },
+    },
+  },
+};
+
 export default {
+  name: "SpentTimeChart",
   components: {
     TimeChart,
   },
   data() {
     return {
+      chartType: type,
+      chartTitle: title,
       chartData: {
         labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
         datasets: [
           {
             label: "Spent Time",
-            data: [
-              2.8, 2.2, 3.3, 2.75,
-              4.3, 4.6
-            ], // Replace with your actual data
+            // Initialize with empty array
+            data: [],
             fill: false,
             borderCapStyle: 'round',
             borderColor: "red",
-            // backgroundColor: "rgba(199, 210, 254, 0.2)",
             pointBackgroundColor: "red",
             pointBorderColor: "red",
             cubicInterpolationMode: 'monotone',
             pointHoverBorderWidth: 3,
-            pointRadius: 4,
+            pointRadius: 0,
             pointHoverBackgroundColor: "transparent",
             pointHoverRadius: 6,
             tension: 0.4,
+            spanGaps: true,
           },
           {
             label: "Work Load",
-            data: [
-              1.4, 2.75, 2.0, 3.3,
-              1.95, 3.6, 3.8
-            ], // Replace with your actual data
+            // Initialize with empty array
+            data: [],
             fill: true,
             borderCapStyle: 'round',
-            // borderColor: "rgba(75, 192, 192, 1)",
             borderColor: "rgba(199, 210, 254, 1)",
             borderWidth: 2,
             backgroundColor: "rgba(199, 210, 254, 0.2)",
-            // pointBackgroundColor: "rgba(75, 192, 192, 1)",
             pointBorderColor: "rgba(199, 210, 254, 1)",
             cubicInterpolationMode: 'monotone',
             pointRadius: 0,
             pointHoverRadius: 0,
             tension: 0.4,
+            spanGaps: true,
           },
         ],
       },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        layout: { padding: 0 },
-        scales: {
-          x: {
-            display: false,
-            grid: { display: false },
-            ticks: {
-              color: "rgba(0, 0, 0, 0.5)",
-              font: {
-                size: 12,
-                weight: 600,
-              },
-            },
-          },
-          y: {
-            beginAtZero: true,
-            display: false,
-          },
-        },
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            callbacks: {
-              label: (context) => {
-                const value = context.parsed.y;
-                const hours = Math.floor(value);
-                const minutes = Math.round((value - hours) * 60);
-                return ` ${hours}h ${minutes}min`;
-              },
-
-              position: "nearest",
-              // set backgrond to red
-              backgroundColor: "red",
-
-              color: "red",
-              // backgroundColor: "rgba(0, 0, 0, 0.8)",
-            },
-          },
-        },
-      },
+      chartOptions: options,
     };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch("spent-time-data.json");
+        const data = await response.json();
+        // set spent-time data
+        this.chartData.datasets[0].data = data.time.data;
+        // set workload data
+        this.chartData.datasets[1].data = data.workload.data;
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
   },
 };
 </script>
--- src/components/SpentTimeChart/TimeChart.vue
+++ src/components/SpentTimeChart/TimeChart.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="absolute block h-full inset-x-[-8px] bottom-[-8px]">
+  <div class="absolute block h-full -inset-x-2 bottom-[-8px]">
     <canvas ref="timeChart"></canvas>
     <div class="labels absolute inset-x-0 bottom-4 py-2">
-      <div class="flex justify-around text-xs font-semibold text-gray-400">
+      <div class="flex justify-around gap-2 items-center text-xs font-semibold text-gray-400">
         <span>Sun</span>
         <span>Mon</span>
         <span>Tue</span>
@@ -19,19 +19,49 @@
 import Chart from "chart.js/auto";
 
 export default {
-  props: ["chartData", "options"],
+  props: {
+    chartTitle: String,
+    chartType: String,
+    chartData: Object,
+    chartOptions: Object,
+    chartId: String,
+  },
+  methods: {
+    renderChart() {
+      if (!this.timeChart) {
+        this.timeChart = new Chart(this.$refs.timeChart.getContext("2d"), {
+          type: this.chartType,
+          data: this.chartData,
+          options: this.chartOptions,
+        });
+      } 
+      else {
+        this.timeChart.update({
+          data: this.chartData,
+          options: this.chartOptions,
+        });
+      }
+    },
+  },
+  watch: {
+    chartData: {
+      handler(newData) {
+        this.renderChart(this.chartType, newData, this.chartOptions);
+      },
+      deep: true,
+    },
+  },
   mounted() {
-    const ctx = this.$refs.timeChart.getContext("2d");
-
-    new Chart(ctx, {
-      type: "line",
-      data: this.chartData,
-      options: this.options,
-    });
+    this.renderChart(this.chartType, this.chartData, this.chartOptions);
+  },
+  beforeDestroy() {
+    if (this.timeChart) {
+      this.timeChart.destroy();
+    }
   },
 };
 </script>
 
-<style>
+<style scoped>
 /* Add any custom styles here */
 </style>
