22c71ce46cd35e1d0c49066c5e82b88b6da218ec
ZikaZaki <17160061@su.edu.ye>
2023-12-31 19:26:03 +0300

    update CreditCard padding

--- src/components/SideBar/CreditCard.vue
+++ src/components/SideBar/CreditCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="cardContainer" class="flex w-full min-h-[160px] max-w-sm self-center h-44 items-center justify-center">
+  <div id="cardContainer" class="flex w-full min-h-[160px] max-w-sm h-48 lg:h-44 self-center items-center justify-center">
     <div id="card" class="group w-full h-full text-white cursor-pointer">
       <div id="cardInner" class="text-white font-[inconsolata] text-shadow rounded-2xl shadow-md shadow-black/40 w-full h-full relative transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
         <div id="cardFront" class="w-full h-full rounded-xl bg-gradient-to-tr from-blue-800 to-pink-600 absolute top-0 left-0 py-4 px-4 overflow-hidden z-[1]">
@@ -8,13 +8,13 @@
             <img src="/src/assets/chip.png" alt="card-chip" class="w-10 h-7">
             <img src="/src/assets/visa.png" alt="visa-logo" class="w-12">
           </div>
-          <div id="row" class="flex items-center justify-around text-lg mt-9 leading-none">
+          <div id="row" class="flex items-center justify-around text-lg mt-12 lg:mt-9 leading-none">
             <p>5422</p>
             <p>2150</p>
             <p>2858</p>
             <p>6420</p>
           </div>
-          <div id="row" class="flex items-center justify-between text-sm font-light text-justify opacity-80 mt-6 uppercase">
+          <div id="row" class="flex items-center justify-between text-sm font-light text-justify opacity-80 mt-8 lg:mt-6 uppercase">
             <p>CARD HOLDER</p>
             <p>VALID TILL</p>
           </div>
@@ -34,7 +34,7 @@
               <span class="text-black text-sm font-semibold">284</span>
             </div>
           </div>
-          <div id="row" class="flex items-center justify-end mt-6">
+          <div id="row" class="flex items-center justify-end mt-9 lg:mt-8">
             <img src="/src/assets/visa.png" alt="visa-logo" class="w-12">
           </div>
         </div>
